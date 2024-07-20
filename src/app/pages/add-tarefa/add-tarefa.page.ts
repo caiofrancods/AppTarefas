dbe968e48f1936c7f52cbe1405c3a711a872fabe
Caio Franco Gonçalves <francocaio80@gmail.com>
2024-07-20 00:08:52 -0300

    Edição Geral das Páginas
    Inserção dos labels, inputs, entre outros campos necessários nas páginas.

---
+++ src/app/pages/add-tarefa/add-tarefa.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tarefa',
+  templateUrl: './add-tarefa.page.html',
+  styleUrls: ['./add-tarefa.page.scss'],
+})
+export class AddTarefaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
