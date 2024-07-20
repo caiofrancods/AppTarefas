0e99956b23c51ab73cdec744c01d7369c39639a1
Caio Franco Gonçalves <francocaio80@gmail.com>
2024-07-19 23:28:34 -0300

    First

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ATIVIDADE03_20222002869_20222012382',
+  webDir: 'www'
+};
+
+export default config;
